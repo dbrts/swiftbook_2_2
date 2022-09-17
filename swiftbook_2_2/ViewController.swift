57f3fa1239328f0cc9a3d23016331363f2bdc561
Gennady Dobretsov <dobrets@gmail.com>
2022-09-17 07:55:22 +0600

    Initial Commit

---
+++ swiftbook_2_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftbook_2_2
+//
+//  Created by dobrets on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
