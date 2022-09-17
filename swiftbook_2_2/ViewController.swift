d682a24fff0fd94a3125b10e29f369e3f3824d38
Gennady Dobretsov <dobrets@gmail.com>
2022-09-17 09:22:30 +0600

    stackVIew doesn't show

--- swiftbook_2_2/ViewController.swift
+++ swiftbook_2_2/ViewController.swift
@@ -8,16 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var redLightView: UIView!
-    @IBOutlet var yellowLightView: UIView!
-    @IBOutlet var greenLightView: UIView!
-    @IBOutlet var actionButton: UIButton!
+
+    
+    enum TrafficLight {
+        case red, yellow, green, off
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+
     }
 
-
+    
+    func setTrafficLight(for state: TrafficLight) {
+//        switch state{
+//        case .red:
+//            redLightView.alpha = 1
+//            yellowLightView.alpha = 0.5
+//            greenLightView.alpha = 0.5
+//        case .yellow:
+//            redLightView.alpha = 0.5
+//            yellowLightView.alpha = 1
+//            greenLightView.alpha = 0.5
+//        case .green:
+//            redLightView.alpha = 0.5
+//            yellowLightView.alpha = 0.5
+//            greenLightView.alpha = 1
+//        case .off:
+//            redLightView.alpha = 0.5
+//            yellowLightView.alpha = 0.5
+//            greenLightView.alpha = 0.5
+//        }
+    }
 }
 
